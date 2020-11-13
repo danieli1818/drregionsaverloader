3104ab48c0f0d14af354be4a8208017ee3506d09
BuildTools <unconfigured@null.spigotmc.org>
2020-11-13 19:05:33 +0200

    Finished 1.16.2

--- src/main/java/me/danieli1818/dr/region_saver_loader/utils/RegionSerializable.java
+++ src/main/java/me/danieli1818/dr/region_saver_loader/utils/RegionSerializable.java
@@ -11,18 +11,14 @@ import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
-import org.bukkit.craftbukkit.v1_12_R1.CraftWorld;
 
-import com.sk89q.worldedit.Vector;
-import com.sk89q.worldedit.Vector2D;
-import com.sk89q.worldedit.bukkit.BukkitUtil;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.regions.CuboidRegion;
 import com.sk89q.worldedit.regions.Region;
 import com.sk89q.worldedit.world.World;
 
 import me.danieli1818.dr.region_saver_loader.RegionSaverLoader;
-import net.minecraft.server.v1_12_R1.ChunkSection;
-import net.minecraft.server.v1_12_R1.IBlockData;
 
 public class RegionSerializable implements ConfigurationSerializable {
 
@@ -35,8 +31,8 @@ public class RegionSerializable implements ConfigurationSerializable {
 	
 	public Map<String, Object> serialize() {
 		Map<String, Object> serialization = new HashMap<String, Object>();
-		Vector minimumPoint = getMinimumPoint();
-		Vector maximumPoint = getMaximumPoint();
+		BlockVector3 minimumPoint = getMinimumPoint();
+		BlockVector3 maximumPoint = getMaximumPoint();
 		serialization.put("world", getWorld().getName());
 		serialization.put("min_x", minimumPoint.getBlockX());
 		serialization.put("min_y", minimumPoint.getBlockY());
@@ -56,9 +52,9 @@ public class RegionSerializable implements ConfigurationSerializable {
 		int max_x = (Integer) serialization.get("max_x");
 		int max_y = (Integer) serialization.get("max_y");
 		int max_z = (Integer) serialization.get("max_z");
-		World world = BukkitUtil.getLocalWorld(Bukkit.getWorld(worldName));
-		Vector minimumPoint = new Vector(min_x, min_y, min_z);
-		Vector maximumPoint = new Vector(max_x, max_y, max_z);
+		World world = BukkitAdapter.adapt(Bukkit.getWorld(worldName));
+		BlockVector3 minimumPoint = BlockVector3.at(min_x, min_y, min_z);
+		BlockVector3 maximumPoint = BlockVector3.at(max_x, max_y, max_z);
 		Region region = new CuboidRegion(world, minimumPoint, maximumPoint);
 		Bukkit.getScheduler().scheduleSyncDelayedTask(RegionSaverLoader.getPlugin(RegionSaverLoader.class), new Runnable() {
 			
@@ -70,11 +66,11 @@ public class RegionSerializable implements ConfigurationSerializable {
 		return new RegionSerializable(region);
 	}
 	
-	public Vector getMinimumPoint() {
+	public BlockVector3 getMinimumPoint() {
 		return this.region.getMinimumPoint();
 	}
 	
-	public Vector getMaximumPoint() {
+	public BlockVector3 getMaximumPoint() {
 		return this.region.getMaximumPoint();
 	}
 	
@@ -82,19 +78,19 @@ public class RegionSerializable implements ConfigurationSerializable {
 		return this.region.getWorld();
 	}
 	
-	public boolean contains(Vector vector) {
+	public boolean contains(BlockVector3 vector) {
 		return this.region.contains(vector);
 	}
 	
 	public boolean contains(Location location) {
-		Vector vector = new Vector(location.getX(), location.getY(), location.getZ());
+		BlockVector3 vector = BlockVector3.at(location.getX(), location.getY(), location.getZ());
 		return (this.region.getWorld().getName() == location.getWorld().getName()) && contains(vector);
 	}
 	
 	public void replace(final Material prevMaterial, final Material newMaterial) {
-		this.region.forEach(new Consumer<Vector>() {
+		this.region.forEach(new Consumer<BlockVector3>() {
 
-			public void accept(Vector t) {
+			public void accept(BlockVector3 t) {
 				Location location = new Location(Bukkit.getWorld(region.getWorld().getName()), t.getBlockX(), t.getBlockY(), t.getBlockZ());
 				
 				if (location.getBlock().getType() == prevMaterial) {
@@ -111,9 +107,9 @@ public class RegionSerializable implements ConfigurationSerializable {
 		
 		final Map<String, List<Location>> blocksMaterialsMap = new HashMap<String, List<Location>>();
 
-		this.region.forEach(new Consumer<Vector>() {
+		this.region.forEach(new Consumer<BlockVector3>() {
 
-			public void accept(Vector t) {
+			public void accept(BlockVector3 t) {
 				Location location = new Location(Bukkit.getWorld(region.getWorld().getName()), t.getBlockX(), t.getBlockY(), t.getBlockZ());
 				
 				String material = location.getBlock().getType().toString();
@@ -146,24 +142,6 @@ public class RegionSerializable implements ConfigurationSerializable {
 			}
 		}
 		return true;
-	}
-	
-	public static void setBlockInNativeDataPalette(org.bukkit.World world, int x, int y, int z, int blockId, byte data, boolean applyPhysics) {
-	    net.minecraft.server.v1_12_R1.World nmsWorld = ((CraftWorld) world).getHandle();
-	    net.minecraft.server.v1_12_R1.Chunk nmsChunk = nmsWorld.getChunkAt(x >> 4, z >> 4);
-	    IBlockData ibd = net.minecraft.server.v1_12_R1.Block.getByCombinedId(blockId + (data << 12));
-
-	    ChunkSection cs = nmsChunk.getSections()[y >> 4];
-	    if (cs == nmsChunk.a) {
-	        cs = new ChunkSection(y >> 4 << 4, applyPhysics);
-	        nmsChunk.getSections()[y >> 4] = cs;
-	    }
-
-	    if (applyPhysics)
-	        cs.getBlocks().setBlock(x & 15, y & 15, z & 15, ibd);
-	    else
-	    	cs.getBlocks().setBlock(x & 15, y & 15, z & 15, ibd);
-//	       cs.getBlocks().b(x & 15, y & 15, z & 15, ibd);
 	}
 	
 }
